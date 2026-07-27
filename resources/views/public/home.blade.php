0dd4174561db676632c3a9b72f59056a954112cd
Jackm4ss <29736636+Jackm4ss@users.noreply.github.com>
2026-07-27 21:01:50 +0700

    Remove aspirations and articles sections from homepage

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -4,7 +4,5 @@
   @include('public.sections.hero')
   @include('public.sections.about')
   @include('public.sections.activities')
-  @include('public.sections.aspirations')
-  @include('public.sections.articles')
   @include('public.sections.join')
 @endsection

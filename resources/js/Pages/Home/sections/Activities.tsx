94fb551950ccc2d26355bd5379cfcc9281609602
Jackm4ss <29736636+Jackm4ss@users.noreply.github.com>
2026-08-07 03:04:54 +0700

    Fix Jom Sertai marquee: animate single copy when gallery has few photos

--- resources/js/Pages/Home/sections/Activities.tsx
+++ resources/js/Pages/Home/sections/Activities.tsx
@@ -25,7 +25,7 @@ export default function Activities({ gallery }: Props) {
                 </div>
             ) : (
                 <div className="marquee-track" aria-label="Dokumentasi kegiatan">
-                    <div className={`marquee-inner${items.length < 6 ? ' marquee-inner--static' : ''}`}>
+                    <div className={`marquee-inner${items.length < 6 ? ' marquee-inner--few' : ''}`}>
                         {/* First set */}
                         {items.map((item, i) => (
                             <a href="/galeri" className="marquee-item" key={`a-${item.id}`} title={item.title}>
